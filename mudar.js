df5a6dc375ada2327c18728a8b83b42d1ae8a9c1
felindo583 <felindo2203@gmail.com>
2024-10-07 16:24:54 -0300

    clonestarbucks

---
+++ mudar.js
@@ -0,0 +1,7 @@
+function ToggleMenu(){
+var menuToggle = document.querySelector('.toggle');
+var links = document.querySelector('.links');
+menuToggle.classList.toggle('active');
+links.classList.toggle('active')
+
+}
